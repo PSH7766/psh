9e51e203d52fdf19844d6aecc49c5f986f9b833a
PSH7766 <yakup1@naver.com>
2022-08-25 14:31:59 +0900

    test

---
+++ src/main/java/com/example/psh_test/PshTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.psh_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PshTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PshTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/psh_test/PshTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.psh_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PshTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
